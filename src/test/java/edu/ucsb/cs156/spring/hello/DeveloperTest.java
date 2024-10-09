5ea435fa1627fcfd1834bf7bbc5bea4b53ce0cbc
yungong <yungong@ucsb.edu>
2024-10-10 01:42:42 +0800

    xy - updated with actual team member

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -29,9 +29,25 @@ public class DeveloperTest {
     // 100% mutation coverage (all mutants timed out or killed)
 
     @Test
-    public void getGithubId_returns_correct_name() {
+    public void getGithubId_returns_correct_githubId() {
         assertEquals("YungongWang", Developer.getGithubId());
     }
 
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("f24-12", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Abby"),"Team should contain Abby");
+        assertTrue(t.getMembers().contains("Javin"),"Team should contain Javin");
+        assertTrue(t.getMembers().contains("Maya"),"Team should contain Maya");
+        assertTrue(t.getMembers().contains("Sarvesh"),"Team should contain Sarvesh");
+        assertTrue(t.getMembers().contains("Yixuan"),"Team should contain Yixuan");
+        assertTrue(t.getMembers().contains("Yungong"),"Team should contain Yungong");
+    }
 
 }
